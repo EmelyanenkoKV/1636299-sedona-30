16274b90371d30ffbf8dc04f8f7e6913f12c219b
EmelyanenkoKV <emelyanenkoKV@gmail.com>
2021-03-11 15:35:04 +0200

    Подключение минимизированных файлов.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const buttonBig = document.querySelector(".button-big");
-const searchForm = document.querySelector(".search-form")
+const searchForm = document.querySelector(".search-form");
+const dateIn = searchForm.querySelector("[name = date-in]");
 
 buttonBig.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -8,104 +9,17 @@ buttonBig.addEventListener("click", function (evt) {
 
 buttonBig.onclick = function () {
   searchForm.classList.toggle("modal-close");
-  searchForm.classList.toggle("modal-show");
-}
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (searchForm.classList.contains("modal-close")) {
-      evt.preventDefault();
-      searchForm.classList.remove("modal-close");
-    }
-  }
-  });
-
-/*
-searchForm.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (searchForm.classList.contains("modal-show")) {
-      evt.preventDefault();
-      searchForm.classList.remove("modal-show");
-    }
-  }
-);*/
-
-/*
-
-const searchForm = document.querySelector(".search-form");
-const buttonBig = document.querySelector(".button-big");
-const dateIn = searchForm.querySelector("[name = date-in]");
-const dateOut = searchForm.querySelector("[name = date-out]");
-const adults = searchForm.querySelector("[name = adults]");
-const kids = searchForm.querySelector("[name = kids]");
-const form = searchForm.querySelector("form");
-let isStorageSupport = true;
-let storage = "";
-
-try {
-  storage = localStorage.getItem("dateIn");
-} catch (err) {
-  isStorageSupport = false;
-}
-buttonBig.addEventListener("click", function (evt) {
-  evt.preventDefault();
   searchForm.classList.toggle("modal-show");
   dateIn.focus();
-  if (storage) {
-    dateIn.value = storage;
-    dateOut.focus();
-  } else {
-    dateIn.focus();
+}
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!searchForm.classList.contains("modal-close")) {
+      event.preventDefault();
+      searchForm.classList.add("modal-close");
+    }
   }
 });
 
-buttonBig.onclick = function () {
-  searchForm.classList.toggle("modal-close");
-  searchForm.classList.remove("modal-error");
-  searchForm.classList.toggle("modal-show");
-}
-try {
-  storageAdult = localStorage.getItem("adult");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-try {
-  storageKids = localStorage.getItem("kids");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-form.addEventListener("submit", function (evt) {
-  if (!dateIn.value || !dateOut.value || !adults.value || !kids.value) {
-    evt.preventDefault();
-    searchForm.classList.remove("modal-error");
-    searchForm.offsetWidth = searchForm.offsetWidth;
-    searchForm.classList.add("modal-error");
-
-    /!*console.log("Введите данные");*!/ /!*Эту строку нужно оставлять?*!/
-
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("dataIn", dateIn.value);
-      localStorage.setItem("dataIn", dateOut);
-      localStorage.setItem("dataIn", adults.value);
-      localStorage.setItem("dataIn", kids.value);
-    }
-
-};
-
-/!* Окно не закрывается с клавиши ESC *!/
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (searchForm.classList.contains("modal-show")) {
-      evt.preventDefault();
-      searchForm.classList.remove("modal-show");
-      searchForm.classList.remove("modal-error");
-    }
-  }
-);
-
-*/
 
