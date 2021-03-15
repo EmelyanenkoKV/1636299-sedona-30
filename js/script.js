58dd1154238cb40dab3a1bb68dee8493b8664d24
EmelyanenkoKV <emelyanenkoKV@gmail.com>
2021-03-15 13:22:05 +0200

    Подготовка к защите. Внесение правок.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 const buttonBig = document.querySelector(".button-big");
 const searchForm = document.querySelector(".search-form");
 const dateIn = searchForm.querySelector("[name = date-in]");
+const dateOut = searchForm.querySelector("[name = date-out]");
+const Adults = searchForm.querySelector("[name = adults]");
+const Kids = searchForm.querySelector("[name = kids]");
+/*const mapImg = document.querySelector(".map-img");
+const mapIframe = document.querySelector(".map-iframe");
+
+document.addEventListener("DOMContentLoaded", function () {
+  mapImg.classList.add("map-hidden");
+  mapIframe.classList.remove("map-hidden");
+});*/
+searchForm.addEventListener("submit", function(evt) {
+  if (!dateIn.value || !dateOut.value || !Adults.value || !Kids.value) {
+    evt.preventDefault();
+    searchForm.classList.remove("modal-error");
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add("modal-error");
+  }
+});
 
 searchForm.classList.add("modal-close");
 
 buttonBig.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchForm.classList.toggle("modal-show");
+  searchForm.classList.remove("modal-error");
 });
 
 buttonBig.onclick = function () {
@@ -20,6 +39,7 @@ window.addEventListener("keydown", function (event) {
     if (!searchForm.classList.contains("modal-close")) {
       event.preventDefault();
       searchForm.classList.add("modal-close");
+      searchForm.classList.remove("modal-error");
     }
   }
 });
